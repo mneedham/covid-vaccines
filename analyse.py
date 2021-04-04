9a52dd489798d5dc877da14cd6b2c2e80e6bff6d
Mark Needham <m.h.needham@gmail.com>
2021-04-04 18:09:00 +0100

    4 April 2021

--- analyse.py
+++ analyse.py
@@ -14,7 +14,7 @@ def custom_strftime(format, t):
 population = 68134973
 alt.themes.enable('fivethirtyeight')
 
-latest_date = parser.parse("2021-04-03")
+latest_date = parser.parse("2021-04-04")
 
 dose1 = pd.read_csv(f"data/data_{latest_date.strftime('%Y-%b-%d')}-dose1.csv")
 dose2 = pd.read_csv(f"data/data_{latest_date.strftime('%Y-%b-%d')}-dose2.csv")
