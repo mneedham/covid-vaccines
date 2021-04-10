d99b605dd9ea1fa14125cb3765d2fa9938db2e3d
Mark Needham <m.h.needham@gmail.com>
2021-04-10 17:35:13 +0100

    updates

--- app.py
+++ app.py
@@ -179,19 +179,17 @@ def overview(latest_daily_date, latest_weekly_date):
     spreadsheet = f"data/COVID-19-weekly-announced-vaccinations-{latest_weekly_date.strftime('%-d-%B-%Y')}.xlsx"
     total = dt.total_vaccination_rates(spreadsheet)
     total.loc[:, "%"] = (total.loc[:, "%"] * 100)
-    total.loc[:, "Population"] = total["Population"].map('{:,d}'.format)
-
-    total.loc[:, "VaccinationsFormatted"] = total["Vaccinations"].map('{:,d}'.format)
-
+    # total.loc[:, "Population"] = total["Population"].map('{:,d}'.format)
+    
     st.header("By Age Group")    
-    st.table(total.drop(["Age"], axis=1))
+    st.table(total.drop(["Age"], axis=1).style.format({"Population": "{:,d}", "Vaccinations": "{:,d}"}))
 
     left, right = st.beta_columns(2)
 
     with left:
         total_doses_chart = alt.Chart(total, padding={"left": 10, "top": 10, "right": 10, "bottom": 10}).mark_bar().encode(
             y=alt.Y('Age', sort=["index"]),
-            x=alt.X('%', scale=alt.Scale(domain=[0, 100])),    
+            x=alt.X('%', scale=alt.Scale(domain=[0, 100]), title="% of people vaccinated"),    
             tooltip=["Age", alt.Tooltip('%', format='.2f')] 
         ).properties(title="Percentage vaccinated")
         st.altair_chart(total_doses_chart, use_container_width=True)    
@@ -199,7 +197,7 @@ def overview(latest_daily_date, latest_weekly_date):
     with right:
         total_doses_chart2 = alt.Chart(total, padding={"left": 10, "top": 10, "right": 10, "bottom": 10}).mark_bar().encode(
             y=alt.Y('Age', sort=["index"]),
-            x=alt.X('Vaccinations'),    
+            x=alt.X('Vaccinations', title="# of people vaccinated"),    
             tooltip=["Age", alt.Tooltip('Vaccinations')] 
         ).properties(title="People vaccinated")
         st.altair_chart(total_doses_chart2, use_container_width=True)    
