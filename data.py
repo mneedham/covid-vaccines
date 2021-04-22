642943c66afb2fae785267189c5ab5007bd9562e
Mark Needham <m.h.needham@gmail.com>
2021-04-22 19:11:05 +0100

    spreadsheet structure changed

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import numpy as np
 from dateutil import parser
 from utils import suffix, custom_strftime
 
-# @st.cache 
+@st.cache 
 def create_vaccines_dataframe(latest_date):
     dose1 = pd.read_csv(f"data/data_{latest_date.strftime('%Y-%b-%d')}-dose1.csv")
     dose2 = pd.read_csv(f"data/data_{latest_date.strftime('%Y-%b-%d')}-dose2.csv")
@@ -28,22 +28,22 @@ def create_vaccines_dataframe(latest_date):
 
 @st.cache
 def vaccinations_dataframe(spreadsheet):
-    vaccinations = pd.read_excel(spreadsheet, "LTLA", usecols="B:M")
-    columns = np.concatenate((vaccinations.loc[10,:][:4].values, vaccinations.loc[11,:][4:].values), axis=None)
+    vaccinations = pd.read_excel(spreadsheet, "LTLA", usecols="F:P")
+    columns = np.concatenate((vaccinations.loc[10,:][:2].values, vaccinations.loc[11,:][2:].values), axis=None)
     vaccinations = vaccinations.loc[14:327,]
     vaccinations.columns = columns
-    vaccinations = vaccinations.drop(["Region Code (Administrative)", "Region Name (administrative)"], axis=1)
+    # vaccinations = vaccinations.drop(["Region Code (Administrative)", "Region Name (administrative)"], axis=1)
     vaccinations = vaccinations.convert_dtypes()
     return vaccinations    
 
 @st.cache
 def population_dataframe(spreadsheet):
-    population = pd.read_excel(spreadsheet, "Population estimates (NIMS)", usecols="B:L")
+    population = pd.read_excel(spreadsheet, "Population estimates (NIMS)", usecols="D:O")
     population_columns = np.concatenate((population.loc[10,:][:2], population.loc[11, :][2:]), axis=None)
     population = population.loc[14:327,]
     population.columns = population_columns
-    population.insert(loc=2, column="Under 50", value=population["Under 16"] + population["16-49"])
-    population = population.drop(["Under 16", "16-49"], axis=1)
+    population.insert(loc=2, column="Under 45", value=population["Under 16"] + population["16-44"])
+    population = population.drop(["Under 16", "16-44"], axis=1)
     population = population.convert_dtypes()
     return population
 
